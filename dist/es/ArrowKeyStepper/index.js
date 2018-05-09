960480cb586386e356b1419ebe391ae1f5465e1e
Zach Sierakowski <zachary.sierakowski@visionistinc.com>
2018-05-09 08:11:34 -0400

    Fixed multi-sort bug

---
+++ dist/es/ArrowKeyStepper/index.js
@@ -0,0 +1,5 @@
+export {default} from './ArrowKeyStepper';
+
+export {default as ArrowKeyStepper} from './ArrowKeyStepper';
+import {bpfrpt_proptype_ScrollIndices} from './types';
+export {bpfrpt_proptype_ScrollIndices};
